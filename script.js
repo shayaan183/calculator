53b30d8f09072f85804d3fd46259554d2ec9ff76
Shayaan <shayaan.musthafa2005@gmail.com>
2025-09-05 14:19:57 +0400

    Implemented basic functions to display numbers and evaluate their operations, only works for 1 pair of numbers and operator

--- script.js
+++ script.js
@@ -1,6 +1,39 @@
 let firstNumber;
 let secondNumber;
 let currentOperator;
+let currentInput = "";
+
+const display = document.querySelector("#display");
+const numberBtns = document.querySelectorAll(".number-btn");
+const operatorBtns = document.querySelectorAll(".operator-btn")
+
+numberBtns.forEach(button => {
+    button.addEventListener('click', () => appendValue(button.value));
+});
+
+operatorBtns.forEach(button => {
+    button.addEventListener('click', () => handleOperation(button.value));
+})
+
+function appendValue(number) {
+    currentInput += number;
+    display.textContent = currentInput;
+}
+
+function handleOperation(operator) {
+    if (currentOperator == null) {
+        firstNumber = Number(currentInput);
+        currentOperator = operator;
+        currentInput = "";
+        display.textContent = "";
+    } else {
+        if (operator == "=") {
+            secondNumber = Number(currentInput);
+            let finalValue = operate(currentOperator, firstNumber, secondNumber);
+            display.textContent = finalValue;
+        }
+    }
+}
 
 function add(a, b) {
     return a + b;
